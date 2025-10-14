cf7e3d7522742e2d62368df978809bd8933e6d94
DavidB <david.bernard@fms-ea.com>
2025-10-14 11:21:22 +0200

    Option 1 version working

--- nim_games_opt1.py
+++ nim_games_opt1.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
 
 """Récupère et retourne le nom d'utilisateur"""
 def get_user_name():
     return input("Enter you username: ")
 
-"""Les noms des joueurs sont mis dans une liste pour pouvoir être choisis au hasard ensuite"""
-player1 = get_user_name()
-player2 = get_user_name()
-players = [player1, player2]
-
 
 """Définie le joueur qui commence"""
-def get_first_player():
-    return random.choice([player1, player2])
+def get_first_player(player):
+    return input(f"Who starts ? Comp/{player} ?: ")
+
 
 """Fonction principale de fonctionnement du jeu"""
 def play():
@@ -25,38 +20,50 @@ def play():
     for _ in range(5):
         print("...")
 
-    first_player = get_first_player()
-    second_player = players[1] if first_player == players[0] else players[0]
+    player = get_user_name()
+    comp = "Computer"
 
-    matches1, matches2 = {first_player: 21}, {second_player: 21}
+    starter = get_first_player(player)
+
+    first_player = ""
+
+    match starter:
+        case "Comp":
+            first_player = comp
+        case "Player":
+            first_player = player
+
+    matches = 21
 
     print(f"{first_player} plays first!")
 
     print("Start!")
 
-    while matches1[first_player] | matches2[second_player] > 0:
-        first_player_play = input(f"{first_player}, how many matches do you pick? ")
-        while int(first_player_play) > 4 or int(first_player_play) < 1:
+    # Boucle while permettant de prendre des allumettes tant que le compte d'allumettes est supérieur à zéro.
+    while matches > 0:
+        if matches == 21 and first_player == "Computer":
+            matches -= 4
+            print("Computer picked 4 matches!")
+            print(f"Matches left: {matches}")
+        player_play = input(f"{player}, how many matches do you pick? ")
+        while int(player_play) > 4 or int(player_play) < 1:
             print("You must pick between 1 and 4 matches!")
-            first_player_play = input(f"{first_player}, how many matches do you pick? ")
-        matches1[first_player]  -= int(first_player_play)
-        matches2[second_player] -= int(first_player_play)
-        print(f"Matches left: {matches1[first_player]}")
-        if matches1[first_player] <= 0:
-            print(f"{player2} wins!")
+            player_play = input(f"{player}, how many matches do you pick? ")
+        while int(player_play) > matches:
+            print("Not enough matches left!")
+            player_play = input(f"{player}, how many matches do you pick? ")
+        matches  -= int(player_play)
+        print(f"Matches left: {matches}")
+        if matches <= 0:
+            print(f"{comp} wins!")
             break
-        second_player_play = input(f"{second_player}, how many matches do you pick? ")
-        while int(second_player_play) > 4 or int(second_player_play) < 1:
-            print("You must pick between 1 and 4 matches!")
-            second_player_play = input(f"{second_player}, how many matches do you pick? ")
-        matches2[second_player] -= int(second_player_play)
-        matches1[first_player] -= int(second_player_play)
-        print(f"Matches left: {matches1[first_player]}")
-        if matches2[second_player] <= 0:
-            print(f"{player1} wins!")
+        print(f"Computer picked {5 - int(player_play)} matches!")
+        matches -= (5 - int(player_play))
+        print(f"Matches left: {matches}")
+        if matches <= 0:
+            print(f"{player} wins!")
             break
     return None
-
 
 
 if __name__ == "__main__":
